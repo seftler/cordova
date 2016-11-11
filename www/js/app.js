36be0476150483ce09ea917be5b1460f42939394
Evgeny Gladkov <Evgeny Gladkov>
2016-11-11 22:02:33 +0300

    cordova app run

---
+++ www/js/app.js
@@ -0,0 +1,49 @@
+/*
+ * Please see the included README.md file for license terms and conditions.
+ */
+
+
+// This file is a suggested starting place for your code.
+// It is completely optional and not required.
+// Note the reference that includes it in the index.html file.
+
+
+/*jslint browser:true, devel:true, white:true, vars:true */
+/*global $:false, intel:false app:false, dev:false, cordova:false */
+
+
+
+// This file contains your event handlers, the center of your application.
+// NOTE: see app.initEvents() in init-app.js for event handler initialization code.
+
+// function myEventHandler() {
+//     "use strict" ;
+// // ...event handler code here...
+// }
+
+
+// ...additional event handlers here...
+
+var db = null;
+ 
+document.addEventListener('deviceready', function() {
+  db = window.sqlitePlugin.openDatabase({name: 'demo.db', location: 'default'});
+});
+
+  db.transaction(function(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DemoTable (name, score)');
+    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Alice', 101]);
+    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Betty', 202]);
+  }, function(error) {
+    console.log('Transaction ERROR: ' + error.message);
+  }, function() {
+    console.log('Populated database OK');
+  });
+
+  db.transaction(function(tx) {
+    tx.executeSql('SELECT count(*) AS mycount FROM DemoTable', [], function(tx, rs) {
+      console.log('Record count (expected to be 2): ' + rs.rows.item(0).mycount);
+    }, function(tx, error) {
+      console.log('SELECT error: ' + error.message);
+    });
+  });
